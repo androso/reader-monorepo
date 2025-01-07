eeb46e3eb07beeab64b37c5d41622ab0ca61f02b
anibal andrade <59257555+androso@users.noreply.github.com>
2025-01-07 02:59:58 +0000

    implementing logger middleware

---
+++ src/middleware/logger.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+
+export const log = (message: string, source = "express") => {
+    const formattedTime = new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+    });
+    console.log(`${formattedTime} [${source}] ${message}`);
+};
+
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+    const start = Date.now();
+    const path = req.path;
+
+    res.on("finish", () => {
+        const duration = Date.now() - start;
+        if (path.startsWith("/api")) {
+            let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+            log(logLine);
+        }
+    });
+
+    next();
+};
