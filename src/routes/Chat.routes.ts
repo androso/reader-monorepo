7729e7bf794985a94b79ca96d191909ecb3cc5ed
androso <androso421@gmail.com>
2025-01-29 06:55:17 -0600

    feat(chat): enhance conversation routes and environment type definitions

--- src/routes/Chat.routes.ts
+++ src/routes/Chat.routes.ts
@@ -1,65 +1,156 @@
+import { eq } from "drizzle-orm";
 import { db } from "../db";
 import { Conversations, Messages } from "../db/schema";
+import { authenticate } from "../middleware/auth";
 import { OpenAIService } from "../services/OpenAIServices";
-import { Router } from "express";
+import { Router, Request } from "express";
+
 const router = Router();
+const oaiService = new OpenAIService();
 
-router.post("/:resourceType/:id/conversations/stream", async (req, res) => {
-    const { message } = req.body;
-    if (
-        req.params.resourceType === "book" ||
-        req.params.resourceType === "article"
-    ) {
-        try {
-            res.setHeader("Content-Type", "text/event-stream");
-            res.setHeader("Cache-Control", "no-cache");
-            res.setHeader("Connection", "keep-alive");
+router.post(
+    "/:resourceType/:id/conversations",
+    authenticate,
+    async (req: Request, res) => {
+        const { message } = req.body;
+        if (
+            req.params.resourceType === "book" ||
+            req.params.resourceType === "article"
+        ) {
+            try {
+                res.setHeader("Content-Type", "text/event-stream");
+                res.setHeader("Cache-Control", "no-cache");
+                res.setHeader("Connection", "keep-alive");
 
-            const [conversation] = await db
-                .insert(Conversations)
-                .values({
-                    userId: req.user.id,
-                    title: message.substring(0, 50) + "...",
-                    resourceType: req.params.resourceType,
-                    resourceId: req.params.id,
-                })
-                .returning();
-            shell;
-            // create message
-            await db.insert(Messages).values({
-                conversationId: conversation.id,
-                role: "user",
-                content: message,
-            });
-            const oaiService = new OpenAIService();
-            const textStream = await oaiService.generateStreamResponse(message);
+                const [conversation] = await db
+                    .insert(Conversations)
+                    .values({
+                        userId: "4cf48fc5-36d1-429c-aec1-6d1b24857fdd",
+                        // userId: req.user.id,
+                        title: message.substring(0, 50) + "...",
+                        resourceType: req.params.resourceType,
+                        resourceId: req.params.id,
+                    })
+                    .returning();
+                // create message
+                await db.insert(Messages).values({
+                    conversationId: conversation.id,
+                    role: "user",
+                    content: message,
+                });
+                const textStream =
+                    await oaiService.generateStreamResponse(message);
 
-            let accumulatedResponse = "";
-            for await (const chunk of textStream) {
-                const content = chunk.choices[0]?.delta?.content || "";
-                accumulatedResponse += content;
+                let accumulatedResponse = "";
+                for await (const chunk of textStream) {
+                    const content = chunk.choices[0]?.delta?.content || "";
+                    accumulatedResponse += content;
 
-                // Send chunk to client
-                res.write(`data: ${JSON.stringify({ content })}\n\n`);
+                    // Send chunk to client
+                    res.write(`data: ${JSON.stringify({ content })}\n\n`);
+                }
+                await db.insert(Messages).values({
+                    conversationId: conversation.id,
+                    role: "assistant",
+                    content: accumulatedResponse,
+                });
+                // return the conversation Id
+                res.write("data: [DONE]\n\n");
+                res.send();
+            } catch (e) {
+                console.error("Error in chat stream", e);
+                res.write(
+                    `data: ${JSON.stringify({ error: "An error occurred" })}\n\n`
+                );
             }
-            await db.insert(Messages).values({
-                conversationId: conversation.id,
-                role: "assistant",
-                content: accumulatedResponse,
+        } else {
+            res.status(400).send({
+                error: "Invalid resource type",
             });
-            res.write("data: [DONE]\n\n");
-            res.send();
-        } catch (e) {
-            console.error("Error in chat stream", e);
-            res.write(
-                `data: ${JSON.stringify({ error: "An error occurred" })}\n\n`
-            );
         }
-    } else {
-        res.status(400).send({
-            error: "Invalid resource type",
+    }
+);
+
+router.get(
+    "/:resourceType/:id/conversations",
+    authenticate,
+    async (req: Request, res) => {
+        const conversations = await db
+            .select()
+            .from(Conversations)
+            .where(
+                eq(Conversations.userId, "4cf48fc5-36d1-429c-aec1-6d1b24857fdd")
+            );
+        res.send({
+            conversations,
         });
     }
-});
+);
+
+router.post(
+    "/:resourceType/:rid/conversations/:cid/messages",
+    authenticate,
+    async (req, res) => {
+        const {
+            resourceType,
+            rid: resourceId,
+            cid: conversationId,
+        } = req.params;
+        if (!resourceType || !resourceId || !conversationId) {
+            res.status(400).send({
+                error: "Invalid request",
+            });
+            return;
+        }
+        const { role, message } = req.body;
+
+        await db.insert(Messages).values({
+            conversationId,
+            role,
+            content: message,
+        });
+
+        const textStream = await oaiService.generateStreamResponse(message);
+        let accumulatedResponse = "";
+        for await (const chunk of textStream) {
+            const content = chunk.choices[0]?.delta?.content || "";
+            accumulatedResponse += content;
+            res.write(`data: ${JSON.stringify({ content })}\n\n`);
+        }
+        await db.insert(Messages).values({
+            conversationId,
+            role: "assistant",
+            content: accumulatedResponse,
+        });
+        res.write("data: [DONE]\n\n");
+        res.send();
+    }
+);
+
+router.get(
+    "/:resourceType/:id/conversations/:conversationId",
+    authenticate,
+    async (req: Request, res) => {
+        const conversationId = req.params.conversationId;
+        const [conversation] = await db
+            .select()
+            .from(Conversations)
+            .where(eq(Conversations.id, conversationId));
+        if (!conversation) {
+            res.status(404).send({
+                error: "Conversation not found",
+            });
+            return;
+        }
+        const messages = await db
+            .select()
+            .from(Messages)
+            .where(eq(Messages.conversationId, conversationId));
+
+        res.send({
+            messages,
+        });
+    }
+);
 
 export default router;
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,21 +1,24 @@
-import { User } from "../types";
-declare namespace NodeJS{
-    interface ProcessEnv{
-        DO_SPACES_KEY : string;
-        DO_SPACES_SECRET : string;
-        DO_SPACES_ENDPOINT : string;
-        DO_SPACES_NAME : string;
-        OPENAI_API_KEY : string;
-        CHROMA_URL : string;
-        CHROMA_CLIENT_AUTH_CREDENTIALS : string;
-        DATABASE_URL : string;
-    }
-}
+import { User } from "./index";
 
 declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            DO_SPACES_KEY: string;
+            DO_SPACES_SECRET: string;
+            DO_SPACES_ENDPOINT: string;
+            DO_SPACES_NAME: string;
+            OPENAI_API_KEY: string;
+            CHROMA_URL: string;
+            CHROMA_CLIENT_AUTH_CREDENTIALS: string;
+            DATABASE_URL: string;
+        }
+    }
+
     namespace Express {
         interface Request {
             user: User;
         }
     }
 }
+
+export {};
